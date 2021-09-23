f12a7359ece8db9b7ff97b55444c5e819deb3890
xzx-dalao <1340796532@qq.com>
2021-09-23 22:06:58 +0800

    feat(修改网络请求): 修改网络请求

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -19,9 +19,9 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://coderwhy.org/prod'
+  BASE_URL = '/api'
 } else {
-  BASE_URL = 'http://coderwhy.org/test'
+  BASE_URL = '/api'
 }
 
 export { BASE_URL, TIME_OUT }
