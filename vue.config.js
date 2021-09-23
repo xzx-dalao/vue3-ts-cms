55137c08e5fa6cf25cc2cc4378d54658624219ae
xzx-dalao <1340796532@qq.com>
2021-09-23 21:52:29 +0800

    fix(修改打包文件): 修改build为dist

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const path = require('path')
+// const path = require('path')
 
 module.exports = {
   // 1.配置方式一: CLI提供的属性
-  outputDir: './build',
+  //outputDir: './bulid',
   // publicPath: './',
   devServer: {
     proxy: {
